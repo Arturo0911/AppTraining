d30d6f7ad8155b3d30921b13a48ea08504660f91
Arturo Negreiros <anegreiross@outlook.com>
2021-02-11 21:56:20 -0500

    finished MainPanelActivity

--- app/src/main/java/com/example/trainingregister/MainPanel.java
+++ app/src/main/java/com/example/trainingregister/MainPanel.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.chip.Chip;
 
 public class MainPanel extends AppCompatActivity {
 
-    private Chip chipChineeseFood;
+    private Spinner spinnerTypeTraining;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_panel);
-        chipChineeseFood = (Chip) findViewById(R.id.chipChineeseFood);
 
+        spinnerTypeTraining = (Spinner) findViewById(R.id.spinnerTypeTraining);
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(MainPanel.this, R.array.typetraining, R.layout.support_simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerTypeTraining.setAdapter(arrayAdapter);
     }
 
 
