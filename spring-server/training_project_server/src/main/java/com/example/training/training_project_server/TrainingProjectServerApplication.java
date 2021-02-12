34373e87e7720be799207993d9d72ae3a76c04c3
Arturo Negreiros <anegreiross@outlook.com>
2021-02-11 20:53:49 -0500

    added server

---
+++ spring-server/training_project_server/src/main/java/com/example/training/training_project_server/TrainingProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.training.training_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingProjectServerApplication.class, args);
+	}
+
+}
